79ec8456cbcfa58128f066b539b3128516eaf31b
massaka99 <119759970+massaka99@users.noreply.github.com>
2024-10-22 16:33:17 +0200

    Slettet unødvendige filer + gjort klar til at lave frontenden for data visualering og diverse

--- smart-home-client/src/app/app.component.ts
+++ smart-home-client/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-import { AddcreditcardComponent } from './add-credit-card/add-credit-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink,  RouterLinkActive, ReactiveFormsModule, AddcreditcardComponent],
+  imports: [RouterOutlet, RouterLink,  RouterLinkActive, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- smart-home-client/src/app/app.routes.ts
+++ smart-home-client/src/app/app.routes.ts
@@ -1,17 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { CreditcardlistComponent } from './credit-card-list/credit-card-list.component';
-import { AddcreditcardComponent } from './add-credit-card/add-credit-card.component';
-import { TransactionsComponent } from './transactions/transactions.component';
+
 
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, 
-  { path: 'credit-card-list', component: CreditcardlistComponent },
-  { path: 'add-credit-card', component: AddcreditcardComponent },
-  { path: 'transactions', component: TransactionsComponent },
-  { path: 'transactions', loadComponent: () => import('./transactions/transactions.component').then(m => m.TransactionsComponent),},
-  { path: '**', redirectTo: '/transactions',},
-  { path: 'transaction-list', loadComponent: () => import('./transactions/transaction-list.component').then(m => m.TransactionListComponent),},
-  { path: '**', redirectTo: '/transactions',}
 ];  
